ef04eab691ac6163bbef7b88ef594d1bf5975cc7
Nickolay Vasiliev <nicko.v@me.com>
2015-09-28 16:25:03 +0300

    new object structure and input handling algorithm

--- js/script.js
+++ js/script.js
@@ -33,68 +33,62 @@
 				document.getElementById(step).className = 'h1-success';
 			}
 		}
-		function handleString(str) { // (02)
-			var result = '', catalog = {}, quotes = 0, i;
-			if (str.indexOf('ACCEL_ASCII') === -1) { showError(0, 'firstStep'); return; }
-			if (str.indexOf('$') > -1) { showError(1, 'firstStep'); return; }
-			Object.defineProperties(catalog, {
-				'10': {value: ''},
-				'13': {value: ''},
-				'34': {get: function () {
-					if (str[i - 1] !== '\\') {
-						quotes += 1;
-						if (str[i - 2] === ')') { return '$\"'; }
+		function handleInput(string) {
+			var arr, obj = {}, currLevel = obj;
+			
+			function AddLevel(string) {
+				Object.defineProperties(this, {
+					header: {value: string, enumerable: false},
+					parent: {value: currLevel, enumerable: false}
+				});
+			}
+			function calcBrackets(string) {
+				var brackets = 0, quote = 0, i;
+				for (i = 0; i < string.length; i += 1) {
+					if (string[i] === '\"' && string[i - 1] !== '\\') { quote += 1; }
+					if (quote % 2 === 0) {
+						if (string[i] === '(') { brackets += 1; }
+						if (string[i] === ')') { brackets -= 1; }
 					}
-					return '\"';
-				}},
-				'40': {get: function () { return ([34, 36].indexOf(str.charCodeAt(i - 1)) + 1) ? '(' : '$('; }},
-				'32': {get: function () { return ([10, 13, 32, 36, 41].indexOf(str.charCodeAt(i - 1)) + 1 ||
-																					[10, 13, 40, 41].indexOf(str.charCodeAt(i + 1)) + 1) ? '' : ' '; }},
-				def:  {get: function () { return str[i]; }}
-			});
-			for (i = 0; i < str.length; i += 1) {
-				if (quotes % 2 && str[i] !== '\"') {
-					result += str[i];
+				}
+				return brackets;
+			}
+			
+			arr = string.split('\n').reduce(function (result, str, i, a) {
+				str = str.trim();
+				if (str) {
+					if (str[0] === ')') {
+						result[result.length - 1] += ')';
+					} else {
+						result.push(str);
+					}
+				}
+				return result;
+			}, []);
+			arr.forEach(function (string) {
+				var brackets = calcBrackets(string), n = Object.keys(currLevel).length, i = 1;
+				if (brackets > 0) {
+					currLevel[n] = new AddLevel(string);
+					currLevel = currLevel[n];
 				} else {
-					result += (catalog.hasOwnProperty(str.charCodeAt(i))) ? catalog[str.charCodeAt(i)] : catalog.def;
-				}
-			}
-			return result;
-		}
-		function buildList(array) { // (03)
-			var obj = {'ACCEL_ASCII \"C:\\a.pcb\"': null}, currLevel = obj;
-			function AddLevel() {
-				Object.defineProperty(this, 'parent', {value: currLevel, enumerable: false});
-			}
-			array.forEach(function (str) {
-				var c = 0, s = str.length - 1, i;
-				if ((str[0] === '(' && str[str.length - 1] === ')') ||
-						(str[0] === '\"' && str[str.length - 1] === '\"')) {
-					currLevel[str] = null;
-					while (str.slice(s, s + 1) === ')') {
-						c += 1;
-						s -= 1;
+					currLevel[n] = string;
+					while (i <= Math.abs(brackets)) {
+						currLevel = currLevel.parent;
+						i += 1;
 					}
-					if (c > 1) {
-						for (i = 1; i < c; i += 1) {
-							currLevel = currLevel.parent;
-						}
-					}
-				} else if (str[0] === '(' && str[str.length - 1] !== ')') {
-					while (currLevel.hasOwnProperty(str)) { str += ' '; } // Костыль :(
-					currLevel[str] = new AddLevel();
-					currLevel = currLevel[str];
 				}
 			});
-			if (Object.keys(obj).length < 3) { showError(2, 'firstStep'); }
 			Object.defineProperty(obj, 'convertToArray', {
 				value: function () {
 					var array = [];
 					function walker(object) {
-						var key;
-						for (key in object) {
-							if (object.hasOwnProperty(key)) {
-								if (object[key]) { array.push(key.trim()); walker(object[key]); } else { array.push(key); }
+						var i;
+						for (i = 0; i <= Object.keys(object).length - 1; i += 1) {
+							if (typeof object[i] === 'object') {
+								array.push(object[i].header);
+								walker(object[i]);
+							} else {
+								array.push(object[i]);
 							}
 						}
 					}
@@ -104,11 +98,10 @@
 			});
 			return obj;
 		}
-		
-		content = buildList(handleString(this.result).split('$'));
-		if (error) { return; } else { showError(-1, 'firstStep'); }
-		document.getElementById('step2').style.display = 'flex';
-		window.console.log(content);
+		content = handleInput(this.result);
+		//if (error) { return; } else { showError(-1, 'firstStep'); }
+		//document.getElementById('step2').style.display = 'flex';
 		document.write('<pre>' + content.convertToArray().join('</br>') + '</pre>');
+		console.log(content);
 	};
 }());
